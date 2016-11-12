4c6533abfd5d3b0bea77457f8312909185c2a5a8
Damain Russel <milton.edwards02@mymona.uwi.edu>
2016-11-12 02:40:15 +0000

    Final update

--- request.php
+++ request.php
@@ -4,6 +4,8 @@
 // respond with a value
 
 $query = $_GET['q'];
+$all = $_GET['all'];
+
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
     "bar" => "A place that sells alcholic beverages",
@@ -13,6 +15,27 @@ $definition = [
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
     "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
 ];
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?> 
+    <entries> 
+        <definition name="definition" author="John"> 
+            a statement of the exact meaning of a word, especially in a dictionary. 
+        </definition> 
+        <definition name="bar" author="mary"> 
+            a place that sells alcholic beverages 
+        </definition> 
+        <definition name="ajax" author="Kimberly"> 
+            technique which involves the use of javascript and xml 
+        </definition> 
+</entries>';
 
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+if($all){
+    header('Content-Type: text/xml');
+    $xmlOutput = new SimpleXMLElement($xmldata);
+    echo $xmlOutput->asXML();
+}else{
+    print "<h3>" . strtoupper($query) . "</h3>";
+    print "<p>" . $definition[$query] . "</p>";
+}
+
+
+
